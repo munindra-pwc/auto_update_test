cac97406ce5a20178351e4d2ebdc9dd88fb8752a
munindra <we.munindra321@gmail.com>
2023-02-02 11:25:11 +0000

    added new

--- test.py
+++ test.py
@@ -1,7 +1,7 @@
 from updater4pyi import upd_source, upd_core
 from updater4pyi.upd_iface_pyqt4 import UpdatePyQt4Interface
 
-swu_source = upd_source.UpdateGithubReleasesSource('githubusername/githubproject')
+swu_source = upd_source.UpdateGithubReleasesSource('https://github.com/munindra-pwc/auto_update_test.git')
 swu_updater = upd_core.Updater(current_version=...,
                                update_source=swu_source)
 swu_interface = UpdatePyQt4Interface(swu_updater,
