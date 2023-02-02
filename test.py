d2f516961082dbac4fe5536fd51a1f534b19823a
munindra <we.munindra321@gmail.com>
2023-02-02 11:20:40 +0000

    added

--- test.py
+++ test.py
@@ -1,1 +1,14 @@
-print("Hello")
+from updater4pyi import upd_source, upd_core
+from updater4pyi.upd_iface_pyqt4 import UpdatePyQt4Interface
+
+swu_source = upd_source.UpdateGithubReleasesSource('githubusername/githubproject')
+swu_updater = upd_core.Updater(current_version=...,
+                               update_source=swu_source)
+swu_interface = UpdatePyQt4Interface(swu_updater,
+                                     progname='Your Project Name',
+                                     ask_before_checking=True,
+                                     parent=QApplication.instance())
+
+
+
+print("Hi")
